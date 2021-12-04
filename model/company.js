9af4a1f56acf9f59735b1f79b85aff13243d60af
GANDHI MITESH <91385949+Vnsgum99@users.noreply.github.com>
2021-12-04 11:29:41 +0530

    Update company.js

--- model/company.js
+++ model/company.js
@@ -10,32 +10,3 @@ const companyModel= mongoose.model("fullstack",companySchema,"fullstack");
 module.exports=companyModel;
 
 
-
-
-
-
-
-
-/* /* ompany id : String
-name : String
-product ids : Array of String */
-
-/* const compaines=[
-    {
-        cid=1,
-        cname="abc",
-        pid=["1","4"],
-        
-    },
-    {
-        cid="2",
-        cname="paq",
-        pid=["2","5"],
-    },
-    {
-        cid="3",
-        cname="xyz",
-        pid=["3","6"],
-    },
-]
-module.exports=compaines; */ 
